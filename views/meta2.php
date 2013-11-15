62a87c4b45c2b5c70c4f331830f5de3f9c781af2
Samuel Demirdjian <s@enov.ws>
2013-11-14 23:27:27 -0800

    Add `PHP_EOL` to meta2.php

--- views/meta2.php
+++ views/meta2.php
@@ -6,16 +6,16 @@ if ( ! isset($tags))
 }
 if ( ! Fragment::load('meta:'.var_export($tags, TRUE), Date::MINUTE * 5, TRUE))
 {
-	echo '<!-- meta tags begin -->';
+	echo PHP_EOL . '<!-- meta tags begin -->' . PHP_EOL;
 	if (isset($tags['title']))
 	{
-		echo '<title>'.HTML::chars(implode(' - ', (array) $tags['title'])).'</title>';
+		echo '<title>'.HTML::chars(implode(' - ', (array) $tags['title'])).'</title>' . PHP_EOL;
 		unset($tags['title']);
 	}
 	foreach ($tags as $attributes)
 	{
-		echo '<meta'.HTML::attributes($attributes).'/>';
+		echo '<meta'.HTML::attributes($attributes).'/>' . PHP_EOL;
 	}
-	echo '<!-- meta tags end -->';
+	echo '<!-- meta tags end -->' . PHP_EOL;
 	Fragment::save();
 }
