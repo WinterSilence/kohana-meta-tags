5fa424ac2b5f430bf829dcc48f392fce55182285
Samuel Demirdjian <s@enov.ws>
2013-11-14 23:51:02 -0800

    cache_lifetime for meta2.php

--- views/meta2.php
+++ views/meta2.php
@@ -1,10 +1,15 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
+$cache_lifetime =
+  Kohana::$environment === Kohana::PRODUCTION ? // test the environment
+  $cache_lifetime : // use the cache lifetime value from config
+  0; // no caching for DEVELOPMENT environment
+
 if ( ! isset($tags))
 {
 	$tags = Meta::instance()->get();
 }
-if ( ! Fragment::load('meta:'.var_export($tags, TRUE), Date::MINUTE * 5, TRUE))
+if ( ! Fragment::load('meta:'.var_export($tags, TRUE), $cache_lifetime, TRUE))
 {
 	echo PHP_EOL . '<!-- meta tags begin -->' . PHP_EOL;
 	if (isset($tags['title']))
