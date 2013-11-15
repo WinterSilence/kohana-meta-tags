de8d2c34d3d959262a38b4afa90a79c7201828bf
Samuel Demirdjian <s@enov.ws>
2013-11-14 23:24:58 -0800

    Add `PHP_EOL` to meta.php, easier to read after parsing

--- views/meta.php
+++ views/meta.php
@@ -6,16 +6,16 @@ if ( ! Fragment::load('meta:'.Request::initial()->url(), Date::MINUTE * 5, TRUE)
 	{
 		$tags = Meta::instance()->get();
 	}
-	echo '<!-- meta tags begin -->';
+	echo PHP_EOL. '<!-- meta tags begin -->' . PHP_EOL;
 	if (isset($tags['title']))
 	{
-		echo '<title>'.HTML::chars(implode(' - ', (array) $tags['title'])).'</title>';
+		echo '<title>'.HTML::chars(implode(' - ', (array) $tags['title'])).'</title>' . PHP_EOL;
 		unset($tags['title']);
 	}
 	foreach ($tags as $attributes)
 	{
-		echo '<meta'.HTML::attributes($attributes).'/>';
+		echo '<meta'.HTML::attributes($attributes).'/>' . PHP_EOL;
 	}
-	echo '<!-- meta tags end -->';
+	echo '<!-- meta tags end -->' . PHP_EOL;
 	Fragment::save();
 }
